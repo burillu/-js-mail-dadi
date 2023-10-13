ab913eb2aad594e7a68270b3dbcd17ca99e31446
burillu <burillu90@hotmail.it>
2023-10-13 15:56:54 +0200

    scaffolding, js =>comment for prepare work

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+// Email
+
+// prendo il campo input
+const inputMail= document.getElementById('input-mail');
+const btnInput= document.getElementById('btn-mail');
+btnInput.addEventListener('click', checkMail);
+// array email utente iscritto
+
+// controllo email inserita all'interno dell'input
+
+// stampo risultato presente
+
+// stampo risultato non presente chiedendo di iscriversi
+
+
+// function checkMail
+
+function checkMail(){
+    let mailUser = inputMail.value;
+    console.log(mailUser);
+}
